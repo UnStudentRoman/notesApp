5298a46282ed2a0cdca65e8c3b98fe90067543e8
Cristian Marius <cristianmariusv97@gmail.com>
2023-06-21 17:13:03 +0300

    Adding User Feature.

--- Backend/mynotes/api/urls.py
+++ Backend/mynotes/api/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
 urlpatterns = [
+    
     path('', view=views.getRoutes, name='routes'),
+    path('token/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
     path('notes/', views.getNotes, name='notes'),
     path('notes/create/', views.createNote, name='create-note'),
     path('notes/<str:pk>/', views.getNote, name='note'),
     path('notes/<str:pk>/update/', views.updateNote, name='update-note'),
     path('notes/<str:pk>/delete/', views.deleteNote, name='delete-note'),
+
 ]
  
